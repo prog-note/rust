442555ecb49bde40de88c720fffcd5a71322fe81
igor04 <igor.s04g@gmail.com>
2015-03-15 20:01:38 +0200

    Added mod.rs and closure.rs

---
+++ closure.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let first = 1i;
+
+  let closure = |arg: int| -> int {
+    println!("argument {}", arg);
+    println!("out varibale {}", first);
+    arg + first
+  };
+
+  println!("closure result - {}", closure(2));
+}
---
+++ mod.rs
@@ -0,0 +1,33 @@
+mod my {
+  // private function
+  fn private_function() {
+    println!("my::private_function()");
+  }
+  // public function
+  pub fn function() {
+    println!("my::function()");
+  }
+
+  // A private module
+  mod private_mod {
+    pub fn function() {
+      println!("super::private_mod::function()");
+    }
+  }
+  // A public module
+  pub mod nested {
+    pub fn function() {
+      super::private_mod::function();
+      println!("my::nested::function()");
+    }
+  }
+}
+
+// mod my;      -- load my mod from `my.rs` or `my/mod.rs`
+// use my::mod; -- include module
+// use something = my::nested::function; -- assign method
+
+fn main() {
+  my::function();
+  my::nested::function();
+}
