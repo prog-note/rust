e45c3d196e89f0692a13b9e1e179a5decdd8137c
igor04 <igor.s04g@gmail.com>
2015-03-29 17:17:19 +0300

    Update let.rs

--- let.rs
+++ let.rs
@@ -2,16 +2,33 @@ fn main() {
     let first = 1;
     println!("immutable variable:  {}", first);
 
-    let mut second = 2;
-    println!("mutable variable: {}", second);
+    // `_` - suppress `unused` warning
+    let mut _second = 2;
+    println!("mutable variable: {}", _second);
+
 
     let (x, y, z) = (1, 2, 3);
     let (x, y, z) = (z, y, x);
     println!("parallel assigning - x: {}, y: {}, z: {}", x, y, z);
 
-    // `let` iside `if` for destruction object
+
+    // `let` inside `if` for destruction object
     let number = Some(7);
     if let Some(i) = number {
         println!("Some - {}", i);
     }
+
+
+    // `let` inside block(new scope)
+    let mut main_scope_var = "main";
+    {
+        println!("vars from {} scope are available", main_scope_var);
+        main_scope_var = "inner scope";
+
+        let inner_variable = "inner scope vars are available just in current scope";
+        let main_scope_var = "main scope vars could be shadowed (become inaccessible)";
+        println!("{} ", inner_variable);
+        println!("{}", main_scope_var);
+    }
+    println!("main scope vars could be changed from {}", main_scope_var);
 }
